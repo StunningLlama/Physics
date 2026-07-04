b3d6ab738a4e67d7029c1459ba1e881b5c82d4a7
Brandon <powderthefirst@gmail.com>
2026-07-04 17:49:38 -0600

    Change compliance back to 1.8

--- src/electrodynamics/gui/MenuBuilder.java
+++ src/electrodynamics/gui/MenuBuilder.java
@@ -32,7 +32,7 @@ public class MenuBuilder {
             } else {
                 JMenuItem item = new JMenuItem(entry.getName().split("\\" + extension)[0]);
 
-                item.addActionListener(_ -> {
+                item.addActionListener((ev) -> {
             		SwingUtilities.invokeLater(() -> {
             			readfileFunc.accept(entry);
             		});
